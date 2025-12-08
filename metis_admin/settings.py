e3036447c3ae33b655b564b0b40b7e638454ca72
Salih Eryılmaz <talhasaliheryilmaz@gmail.com>
2025-12-08 18:42:36 +0300

    Set DEBUG=True for development environment

--- metis_admin/settings.py
+++ metis_admin/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False  # Production için False
+DEBUG = True  # Development için True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8gy15^z036tfb9a%#36tgy6ssb==3+@c1)1nh6@!fdowo$%e!n')
